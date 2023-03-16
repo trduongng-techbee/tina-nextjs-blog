ef8fa143c5e35c85c588be5c45c9032acbd2a5a0
tgound1403 <nguyentrieuduong14032002@gmail.com>
2023-03-16 09:53:34 +0700

    add config data

--- .tina/config.js
+++ .tina/config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch =
+  process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
 
 export default defineConfig({
   branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
+  clientId: "f51bb34d-a2f1-4a75-99a6-5519922efee8", // Get this from tina.io
+  token: "075e6258ad2316aeb3762c032973d29ac210c8b7", // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "public",
